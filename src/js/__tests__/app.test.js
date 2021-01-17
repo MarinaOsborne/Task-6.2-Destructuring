cd277c5e1d5853408cd1746eb3cf3aa411f422b4
Marina Osborne <marina08088@mail.ru>
2021-01-17 16:49:32 +0300

    changed 17.01

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,92 +1,56 @@
-import {
-  Character, Bowman, Swordsman, Magician, Undead, Zombie, Daemon,
-} from '../app';
+import removeArray from '../app';
 
-test('проверка длины имени <3', () => {
-  expect(() => {
-    new Character('хз', 'Swordsman', 10, 40);
-  }).toThrow();
-});
+const character = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      // <- обратите внимание, описание "засекречено"
+    },
+  ],
+};
 
-test('проверка длины имени >10', () => {
-  expect(() => {
-    new Character('хзuhhgfhfhfjjjsd', 'Swordsman', 10, 40);
-  }).toThrow();
-});
-
-test('проверка string', () => {
-  expect(() => {
-    new Character(7, 'Swordsman', 10, 40);
-  }).toThrow();
-});
-
-test('проверка Bowman', () => {
-  expect(() => {
-    new Bowman('Петя', 'Вася', 10, 40);
-  }).toThrow();
-});
-
-test('проверка Swordsman', () => {
-  expect(() => {
-    new Swordsman('Петя', 'Вася', 10, 40);
-  }).toThrow();
-});
-
-test('проверка Magician', () => {
-  expect(() => {
-    new Magician('Петя', 'Вася', 10, 40);
-  }).toThrow();
-});
-
-test('проверка Undead', () => {
-  expect(() => {
-    new Undead('Петя', 'Вася', 10, 40);
-  }).toThrow();
-});
-
-test('проверка Zombie', () => {
-  expect(() => {
-    new Zombie('Петя', 'Вася', 10, 40);
-  }).toThrow();
-});
-
-test('проверка Daemon', () => {
-  expect(() => {
-    new Daemon('Петя', 'Вася', 10, 40);
-  }).toThrow();
-});
-
-test('проверка Daemon', () => {
-  const received = new Daemon('Петя', 'Daemon', 30, 20);
-  expect(received.attack).toEqual(10);
-});
-
-test('проверка Zombie', () => {
-  const received = new Zombie('Петя', 'Zombie', 30, 20);
-  expect(received.attack).toEqual(40);
-});
-
-test('проверка Undead', () => {
-  const received = new Undead('Петя', 'Undead', 30, 20);
-  expect(received.attack).toEqual(25);
-});
-
-test('проверка Zombie', () => {
-  const received = new Zombie('Петя', 'Zombie', 30, 20);
-  expect(received.attack).toEqual(40);
-});
-
-test('проверка Magician', () => {
-  const received = new Magician('Петя', 'Magician', 10, 20);
-  expect(received.attack).toEqual(10);
-});
-
-test('проверка Swordsman', () => {
-  const received = new Swordsman('Петя', 'Swordsman', 30, 20);
-  expect(received.attack).toEqual(40);
-});
-
-test('проверка Bowman', () => {
-  const received = new Bowman('Петя', 'Bowman', 30, 20);
-  expect(received.attack).toEqual(25);
+test('Checking the return value', () => {
+  const received = removeArray(character);
+  expect(received).toEqual([
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ]);
+  expect(received).not.toBe([
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ]);
 });
