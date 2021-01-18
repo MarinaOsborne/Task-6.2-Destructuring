2efd4c8ff1a496d17493eabaea3290d0423cb375
Marina Osborne <marina08088@mail.ru>
2021-01-18 19:25:07 +0300

    changed 18.01

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,11 @@
 
 // console.log('worked');
-
-export default function removeArray({ special = [] }) {
-  for (let i = 0; i < special.length; i += 1) {
-    if (special[i].description === undefined) {
-      special[i].description = 'Описание недоступно';
+const character = {};
+export default function removeArray() {
+  for (let i = 0; i < character.special.length; i += 1) {
+    if (character.special[i].description === undefined) {
+      character.special[i].description = 'Описание недоступно';
     }
   }
-  return special;
+  return character.special;
 }
